114498c905771b840c55edbdae83f0fd38a2748c
Kyaw Kyaw Lwin <kyawkyawlwinthant@gmail.com>
2020-12-11 19:24:34 +0630

    kkl first commit

---
+++ src/main/java/com/example/firstcup/FirstCupApplication.java
@@ -0,0 +1,13 @@
+package com.example.firstcup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstCupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstCupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/firstcup/FirstCupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firstcup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstCupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
